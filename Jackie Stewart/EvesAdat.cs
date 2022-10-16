e33647e73c05d0837150f98a29adaa0f19e5f4a6
Sándor Boros <boros.sandor@petrik.hu>
2022-10-16 19:09:03 +0200

    3. feladat

---
+++ Jackie Stewart/EvesAdat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jackie_Stewart
+{/**
+  * jackie.txt állomány sorait tárolja 
+  */
+    internal class EvesAdat
+    {
+        /// <summary>
+        /// a versenyzés éve (year), például: 1-973
+        /// </summary>
+        readonly int year;
+        /// <summary>
+        /// hany versenyen  indult (races), például: ]-8
+        /// </summary>
+        readonly int races;
+        /// <summary>
+        /// hány versenyt  nyert meg (wins), például: s
+        /// </summary>
+        readonly int wins;
+        /// <summary>
+        /// hányszor volt dobogós  eredménye (podíums)' például: 9
+        /// </summary>
+        readonly int podiums;
+        /// <summary>
+        /// hányszor  indult első  helyről  (po1es), például: 4
+        /// </summary>
+        readonly int poles;
+        /// <summary>
+        /// hiínyszor volt övé  a leggyorsabb  kör  (f astests},  például: 1
+        /// </summary>
+        readonly int fastests;
+
+        public EvesAdat(string[] sor)
+        {
+            this.year = int.Parse(sor[0]);
+            this.races = int.Parse(sor[1]);
+            this.wins = int.Parse(sor[2]);
+            this.podiums = int.Parse(sor[3]);
+            this.poles = int.Parse(sor[4]);
+            this.fastests = int.Parse(sor[5]);
+        }
+
+    
+    }
+}
---
+++ Jackie Stewart/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Jackie_Stewart
+{
+    internal class Program
+    {
+        static List<EvesAdat> evesAdatok = new List<EvesAdat>();
+        static void Main(string[] args)
+        {
+            feladat02("jackie.txt");
+            feladat03();
+            Console.WriteLine("\nprogram vége!");
+            Console.Read();
+        }
+
+        private static void feladat03()
+        {
+            Console.WriteLine($"\n3.feladat: {evesAdatok.Count}");
+        }
+
+        /***
+* olvassa be a jackie.txt állomány sorait,  és tárolja az adatokat  
+* egy összetett adatszerkezetben
+*/
+        private static void feladat02(string adatForras)
+        {
+            if (File.Exists(adatForras))
+            {
+                using (StreamReader sr = new StreamReader(adatForras, Encoding.Default))
+                {
+                    sr.ReadLine();
+                    while (!sr.EndOfStream)
+                    {
+                        evesAdatok.Add(new EvesAdat(sr.ReadLine().Split('\t')));
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hiányzó adataforrás!\nA program leáll!");
+                Console.Read();
+                Environment.Exit(0);
+            }
+        }
+    }
+}
